ddf88d12a6b879fb154669c641210c3be85573be
baolinh <baolinhdev@gmail.com>
2023-09-17 03:08:26 +0700

    use reselect

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,14 @@
-export const todoListSelector = (state) => {
-  const todoRemaining = state.todoList.filter((todo) => {
-    return todo.name.includes(state.filters.search);
-  });
+import { createSelector } from "reselect";
 
-  return todoRemaining;
-};
+export const todoListSelector = (state) => state.todoList;
 export const searchTextSelector = (state) => state.filters.search;
+
+export const todoRemainingSelector = createSelector(
+  todoListSelector,
+  searchTextSelector,
+  (todoList, searchText) => {
+    return todoList.filter((todo) => {
+      return todo.name.includes(searchText);
+    });
+  }
+);
